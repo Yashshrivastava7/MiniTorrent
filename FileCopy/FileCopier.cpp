f570beabf163f78eef9af75e64d1305c22d6dee5
Yash Shrivastava <yash.shrivastava77@gmail.com>
2022-08-17 22:11:59 +0530

    Fixed minor Bug

--- FileCopy/FileCopier.cpp
+++ FileCopy/FileCopier.cpp
@@ -1,20 +1,20 @@
 #include<iostream>
-#include<unsitd.h>
+#include<unistd.h>
 #include<fcntl.h>
 
 using namespace std;
 
 int main(int argc,char* argv[]){
-    char buffer[4096];
+    char buffer;
 
-    int First_file,Second_file = 0;
+    int First_file,Second_file;
 
     First_file = open(argv[1],O_RDONLY);
 
     Second_file = open(argv[2],O_WRONLY | O_CREAT);
 
-    while(read(First_file,&buffer,4096)){
-        write(Second_file,&buffer,4096);
+    while(read(First_file,&buffer,1)){
+        write(Second_file,&buffer,1);
     }
 
     cout << "Copy Successful" << endl;
